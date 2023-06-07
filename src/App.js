55be048896515996ca17f25fa49eb3f8a639e5d7
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-06-07 15:40:52 +0530

    added functionality of reset password and other endpoints

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import PatientRequests from "./pages/PatientRequests";
 import HospitalDetails from "./pages/HospitalDetails";
 import HospitalAdd from "./pages/HospitalAdd";
 import Settings from "./pages/Settings";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   const user = useContext(AppContext);
@@ -33,6 +34,7 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
 
         <Route path="/" element={<Dashboard />}>
           <Route path="/" element={<DashboardContent />} />
--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,18 +1,28 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
-const ForgotPassword = () => {
-  const [forgotPassData, setForgotPassData] = useState({
-    email: "",
+const ResetPassword = () => {
+  const Navigate = useNavigate();
+  const { token } = useParams();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const email = searchParams.get("email");
+
+  const [resetPassData, setResetPassData] = useState({
+    email,
+    password: "",
+    password_confirmation: "",
+    token,
   });
 
   const onChangeHandler = (e) => {
     const handlerName = e.target.name;
     const handlerValue = e.target.value;
 
-    setForgotPassData(() => ({
-      ...forgotPassData,
+    setResetPassData(() => ({
+      ...resetPassData,
       [handlerName]: handlerValue,
     }));
   };
@@ -20,26 +30,19 @@ const ForgotPassword = () => {
   const sendDataToAPI = async () => {
     try {
       const res = await axios.post(
-        process.env.REACT_APP_BASE_URL + "forgot-password",
-        {
-          email: forgotPassData.email,
-        }
+        process.env.REACT_APP_BASE_URL + "reset-password",
+        resetPassData
       );
       console.log(res);
 
       toast.success(res.data.status);
+      Navigate("/sign-in");
     } catch (error) {
       console.log(error);
 
       toast.error(error.response.data.message);
       toast.error(error.response.data.email);
     }
-  };
-
-  const submitForgotPassData = () => {
-    sendDataToAPI();
-
-    console.log(forgotPassData);
   };
 
   return (
@@ -53,7 +56,7 @@ const ForgotPassword = () => {
                 <img
                   alt="Midone - HTML Admin Template"
                   className="w-6"
-                  src="dist/images/logo.svg"
+                  src="../dist/images/logo.svg"
                 />
                 <span className="text-white text-lg ml-3">Adhicine</span>
               </a>
@@ -61,7 +64,7 @@ const ForgotPassword = () => {
                 <img
                   alt="Midone - HTML Admin Template"
                   className="-intro-x w-1/2 -mt-16"
-                  src="dist/images/illustration.svg"
+                  src="../dist/images/illustration.svg"
                 />
                 <div className="-intro-x text-white font-medium text-4xl leading-tight mt-10">
                   A few more clicks to
@@ -79,22 +82,39 @@ const ForgotPassword = () => {
             <div className="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
               <div className="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
                 <h2 className="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">
-                  Forgot Password?
+                  Reset Password?
                 </h2>
-                <div className="intro-x mt-2 text-slate-400">
-                  Forgot your password? No problem. Just let us know your email
-                  address and we will email you a password reset link that will
-                  allow you to choose a new one.
-                </div>
                 <div className="intro-x mt-8">
                   <input
                     type="text"
                     className="intro-x login__input form-control py-3 px-4 block"
                     placeholder="Email"
                     name="email"
+                    value={email || ""}
                     onChange={onChangeHandler}
+                    disabled
                   />
                   {/* <p className="form-error">email error</p> */}
+                </div>
+                <div className="intro-x mt-8">
+                  <input
+                    type="password"
+                    className="intro-x login__input form-control py-3 px-4 block"
+                    placeholder="Password"
+                    name="password"
+                    onChange={onChangeHandler}
+                  />
+
+                  {/* <p className="form-error">password error</p> */}
+                </div>
+                <div className="intro-x mt-8">
+                  <input
+                    type="password"
+                    className="intro-x login__input form-control py-3 px-4 block"
+                    placeholder="Confirm Password"
+                    name="password_confirmation"
+                    onChange={onChangeHandler}
+                  />
 
                   {/* <p className="form-error">password error</p> */}
                 </div>
@@ -102,9 +122,9 @@ const ForgotPassword = () => {
                 <div className="intro-x mt-5 xl:mt-8 text-center xl:text-left">
                   <button
                     className="btn btn-primary py-3 px-4 w-full xl:mr-3 align-top"
-                    onClick={submitForgotPassData}
+                    onClick={sendDataToAPI}
                   >
-                    Email Password Reset Link
+                    Reset Password
                   </button>
 
                   {/* <button onClick={notify}>Notify!</button> */}
@@ -119,4 +139,4 @@ const ForgotPassword = () => {
   );
 };
 
-export default ForgotPassword;
+export default ResetPassword;
